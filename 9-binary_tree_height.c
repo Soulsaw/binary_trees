9c77a904f2b4a6f900add588b40e51bb08de16fb
Soulsaw <ssawadogo292@gmail.com>
2023-10-31 22:08:59 +0000

    fix task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,8 +19,8 @@ size_t max(size_t n, size_t m)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (-1);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
 	return (max(binary_tree_height(tree->left),
 		binary_tree_height(tree->right)) + 1);
 }
