9df5ff525c063262839ea9a120069c5a3c31a897
Ololade Victor <oluwabori2014@gmail.com>
2023-10-31 21:00:39 -0700

    Binary tree Update

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - This function checks if a node is a leaf
+ * @node: The node to be checked
+ * Return: An integer value
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - A function that goes through
+ * a binary tree using pre-order traversal
+ * @tree: The binary tree to traverse
+ * @func: The fuction to use
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
