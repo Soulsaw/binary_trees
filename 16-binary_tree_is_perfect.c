c0a94acf030b24674fb21b106141f34366f74792
Soulsaw <ssawadogo292@gmail.com>
2023-11-01 18:37:22 +0000

    task 16 tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,18 +3,38 @@
 #include <math.h>
 #include "binary_trees.h"
 /**
- * maxx - This function return the max of two number
- * @n: The first integer
- * @m: The second integer
- * Return: The max of these two value
+ * binary_tree_height - This function that measures the height of a
+ * binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: 0 or the height of the tree
  */
-size_t maxx(size_t n, size_t m)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t max = n;
+	size_t left_height, right_height;
 
-	if (n < m)
-		max = m;
-	return (max);
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height ? right_height + 1 : left_height + 1);
+}
+/**
+ * binary_tree_size - This function that measures the size of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the size.
+ * Return: 0 or the height of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size, size;
+
+	if (!tree)
+		return (0);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	size = left_size + right_size + 1;
+	return (size);
 }
 /**
  * binary_tree_is_perfect - This  function that checks if a binary tree
@@ -22,15 +42,20 @@ size_t maxx(size_t n, size_t m)
  * @tree: is a pointer to the root node of the tree to check
  * Return: 0 or 1
  */
- int binary_tree_is_perfect(const binary_tree_t *tree)
- {
-	int height, size;
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int height, size, power = 1, i;
 
 	if (!tree)
 		return (0);
 	height = binary_tree_height(tree);
 	size = binary_tree_size(tree);
-	if (size == pow(2, height + 1) - 1)
+
+	for (i = 0; i < height + 1; i++)
+	{
+		power *= 2;
+	}
+	if (size == power - 1)
 		return (1);
 	return (0);
- }
+}
