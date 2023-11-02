05cc1687b1d7ce5614e34a13af6d649c4e905f40
Ololade Victor <oluwabori2014@gmail.com>
2023-11-02 07:28:51 -0700

    Update Binary_Tree

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,23 +1,21 @@
 #include <stddef.h>
-#include <stdbool.h>
 #include "binary_trees.h"
 /**
- * binary_tree_is_bst_util - Utility function to check BST property recursively
+ * binary_util - Utility function to check BST property recursively
  * @tree: Pointer to the root node of the tree
- * @prev: Pointer to the previously visited node's value
+ * @min: Pointer to the minimum allowed value for the current node
+ * @max: Pointer to the maximum allowed value for the current node
  *
  * Return: 1 if tree is a valid BST, 0 otherwise
  */
-int binary_tree_is_bst_util(const binary_tree_t *tree, int *prev)
+int binary_util(const binary_tree_t *tree, const int *min, const int *max)
 {
 	if (tree == NULL)
 		return (1);
-	if (!binary_tree_is_bst_util(tree->left, prev))
+	if ((min && tree->n <= *min) || (max && tree->n >= *max))
 		return (0);
-	if (prev != NULL && tree->n <= *prev)
-		return (0);
-	*prev = tree->n;
-	return (binary_tree_is_bst_util(tree->right, prev));
+	return (binary_util(tree->left, min, &(tree->n)) &&
+		binary_util(tree->right, &(tree->n), max));
 }
 /**
  * binary_tree_is_bst - Checks if a binary tree is a valid BST
@@ -27,7 +25,5 @@ int binary_tree_is_bst_util(const binary_tree_t *tree, int *prev)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int prev = 0;
-
-	return (binary_tree_is_bst_util(tree, &prev));
+	return (binary_util(tree, NULL, NULL));
 }
